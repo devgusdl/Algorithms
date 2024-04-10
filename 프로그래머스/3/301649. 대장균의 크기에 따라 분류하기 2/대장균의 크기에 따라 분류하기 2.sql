edb33350cd73b29deba8ee3b8af6cd196d43befc
Kim Hyun <devgusdl@gmail.com>
2024-04-10 09:10:03 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,8 @@
+select id, (case
+            when ntile(4) over (order by size_of_colony) = 1 then 'LOW'
+            when ntile(4) over (order by size_of_colony) = 2 then 'MEDIUM'
+            when ntile(4) over (order by size_of_colony) = 3 then 'HIGH'
+            else 'CRITICAL'
+           end) as colony_name
+from ecoli_data
+order by id
