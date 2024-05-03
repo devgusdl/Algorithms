e216b1b56d13a0a75da8fe9e9e4988a7a409cf26
Kim Hyun <devgusdl@gmail.com>
2024-05-03 09:19:03 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,7 +1,9 @@
 select item_id, item_name, rarity
 from item_info
 where item_id in (
-    select item_id from item_tree
-        where parent_item_id in (select item_id from item_info where rarity = 'rare')
+    select it.item_id
+    from item_info as ii
+    left join item_tree as it on ii.item_id = it.parent_item_id
+    where ii.rarity='rare' and it.item_id is not null
     )
 order by item_id desc;
